08eeb2f4737de6bd3bec04bb5e0537c831d96215
JanneJansen <jansenjanne@gmail.com>
2019-06-12 16:15:18 +0200

    hover

--- meesterproef.js
+++ meesterproef.js
@@ -3,3 +3,20 @@ var tekst = document.querySelector('#kleding');
 tekst.addEventListener('click', function() {
    tekst.classList.toggle('turn');
     });
+
+
+let jurk = document.getElementById("jurk-path");
+  
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+jurk.addEventListener("mouseover", function( event ) {   
+  // highlight the mouseenter target
+  event.target.style.fill = "url(#img1)";
+//    console.log('jemoeder');
+})
+
+jurk.addEventListener("mouseout", function( event ) { 
+      event.target.style.fill = null;
+})
+    
+//    fill="url(#img1)"
